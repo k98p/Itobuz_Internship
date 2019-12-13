73d9c4f030d2d032701895dd1229fa170e051e9c
k98p <kaustavpaul2018@gmail.com>
2019-12-13 10:42:35 +0530

    finishing_touches

--- async/script.js
+++ async/script.js
@@ -1,17 +1,12 @@
-var test;
-var listarr = [];
 fetch('https://jsonplaceholder.typicode.com/todos')
 	.then(response => response.json())
 	.then(res => {
+		console.log(res);
 		let items = "";
 		for(let i=0; i<res.length; ++i){
-			listarr.push(res[i].id)
 			items += `<input type='checkbox' id="checkbox_${res[i].id}" onclick='check_func("${res[i].id}")'><span id="text_${res[i].id}">${res[i].title}</span></input><br>`
 		}
 		document.getElementById("listdiv").innerHTML = items;
-		console.log(res)
-		test = res 		//doesn't work. Why?
-		return res
   	})
 
 function check_func(n){
