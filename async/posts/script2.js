7900625be1ec82610913f86823a1275a2a602060
k98p <kaustavpaul2018@gmail.com>
2019-12-18 17:48:40 +0530

    implemented promise.all

--- async/posts/script2.js
+++ async/posts/script2.js
@@ -5,6 +5,23 @@ let fetch_url = 'https://jsonplaceholder.typicode.com/posts/' + id;
 let comments_url = 'https://jsonplaceholder.typicode.com/comments?postId=' + id
 //console.log(fetch_url);
 
+const fetch_arr = [fetch(fetch_url), fetch(comments_url)]
+Promise.all(fetch_arr)
+    .then(res=>res.json())
+    .then(data=>{
+        var contain = document.getElementsByClassName("post-container")[0];
+        contain.innerHTML = `<div id="post">
+                                <h3>${data.title} (ID: ${data.id})</h3><h6>${data.body}</h6></div>`;
+        var contain_comment = document.getElementsByClassName("comment-container")[0];
+        for(let i=0; i<data.length; ++i){
+            contain_comment.innerHTML+= '<div style="padding: 10px;"></div>' + '<h5>' + data[i].name + '</h5>' + '<p>' + data[i].body + '</p>' 
+            // /contain_comment.innerHTML+= 
+            //contain_comment.innerHTML+= 
+            //contain_comment.innerHTML+= '</div>'
+        }
+    })
+
+/*
 var contain = document.getElementsByClassName("post-container")[0];
 fetch(fetch_url)
     .then(response => response.json())
@@ -19,9 +36,10 @@ fetch(comments_url)
     .then(response=>response.json())
     .then(data=>{
         for(let i=0; i<data.length; ++i){
-            contain_comment.innerHTML+= '<div style="padding: 10px;"></div>'
-            contain_comment.innerHTML+= '<h5>' + data[i].name + '</h5>'
-            contain_comment.innerHTML+= '<p>' + data[i].body + '</p>'
+            contain_comment.innerHTML+= '<div style="padding: 10px;"></div>' + '<h5>' + data[i].name + '</h5>' + '<p>' + data[i].body + '</p>' 
+            // /contain_comment.innerHTML+= 
+            //contain_comment.innerHTML+= 
             //contain_comment.innerHTML+= '</div>'
         }
     })
+*/
--- async/todos/script.js
+++ async/todos/script.js
@@ -1,83 +1,87 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
+
+var index;
+fetch('http://localhost:3000/todos')
 	.then(response => response.json())
 	.then(res => {
-		console.log(res);
-		let items = "";
-		for(let i=0; i<10; ++i){
-			items += `<label class="container border border-primary mx-auto py-2 rounded-pill" for="checkbox_${res[i].id}">
-							<input type='checkbox' id="checkbox_${res[i].id}" name="checkbox_${res[i].id}" onclick='check_func("${res[i].id}")'>
-								<span id="text_${res[i].id}">${res[i].title}</span>
-							</input>
-						</label><br>`
-		}
-		document.getElementById("listdiv").innerHTML = items;
+		//console.log(res);
+		index = res.length;
+		var todos = ''; 
+		res.forEach((todo)=>{
+			todos = `<label class="container border border-primary mx-auto py-2 rounded-pill" id="label_${todo.id}" for="checkbox_${todo.id}">
+						<input type='checkbox' id="checkbox_${todo.id}" name="checkbox_${todo.id}" onclick='check_func("${todo.id}")'>
+							<span id="text_${todo.id}">${todo.title}</span>
+						</input>
+					</label><br>` + todos
+		})
+		// for(let i=0; i<10; ++i){
+		// 	items += `<label class="container border border-primary mx-auto py-2 rounded-pill" for="checkbox_${res[i].id}">
+		// 					<input type='checkbox' id="checkbox_${res[i].id}" name="checkbox_${res[i].id}" onclick='check_func("${res[i].id}")'>
+		// 						<span id="text_${res[i].id}">${res[i].title}</span>
+		// 					</input>
+		// 				</label><br>`
+		// }
+		document.getElementById("listdiv").innerHTML = todos;
   	})
 
 function check_func(n){
-	var checkid = "checkbox_" + n;
-	var textid = "text_"+n;
-	var checkboxele = document.getElementById(checkid)
-	var textele = document.getElementById(textid)
-	fetch_url = 'https://jsonplaceholder.typicode.com/todos/' + n
-	if (checkboxele.checked){
-		fetch(fetch_url,{
-			method: 'PUT',
-			headers: {
-				"Content-type": "application/json; charset=UTF-8"
-			},
-			body: JSON.stringify({
-				completed: true
-			})
-		}).then(response=>{
-			textele.setAttribute("style" , "text-decoration: line-through;");
-			return response.json()
-		}).then(data=>console.log(data))
-	}
-	else{
-		fetch(fetch_url,{
-			method: 'PUT',
-			headers: {
-				"Content-type": "application/json; charset=UTF-8"
-			},
-			body: JSON.stringify({
-				completed: false
-			})
-		}).then(response=>{
-			textele.setAttribute("style" , "text-decoration: none;");
-			return response.json()	
-		}).then(data=>{
-			console.log(data);
+	// var checkid = "checkbox_" + n;
+	// var textid = "text_"+n;
+	var checkboxele = document.getElementById("checkbox_" + n)
+	var textele = document.getElementById("text_" + n)
+	var labelele = document.getElementById("label_" + n)
+	fetch_url = 'http://localhost:3000/todos/' + n
+	fetch(fetch_url,{
+		method: 'PUT',
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		},
+		body: JSON.stringify({
+			completed: checkboxele.checked
 		})
-	}
+	}).then(response=>{
+		return response.json()
+	}).then(data=>{
+		if(checkboxele.checked){
+			textele.setAttribute("style" , "text-decoration: line-through;");
+			labelele.setAttribute("style", "border-color: #28a745 !important;")
+		}
+		else{
+			textele.setAttribute("style" , "text-decoration: none;");
+			labelele.setAttribute("style", "border-color: #007bff !important;")
+		}
+		console.log(data)
+	}).catch(err=>{
+		console.log(err)
+		labelele.setAttribute("style", "border-color: #dc3545 !important;")
+	})
 }
 
-var docu_list = document.getElementById('listdiv')
-var next_id = 11;
+
 
 function add_func(frm){
-	if (frm.todo_title.value.length = 0){
+	if (frm.todo_title.value.length === 0){
 		alert("Title not added!");
 	}
 	else{
-		//console.log(docu_list)
-		next_id++;
-		fetch('https://jsonplaceholder.typicode.com/todos',{
+		var docu_list = document.getElementById('listdiv')
+		fetch('http://localhost:3000/todos/',{
 			method: 'POST',
 			body: JSON.stringify({
-				userId: frm.todo_userId.value,
-				title: frm.todo_title.value,
-				completed: false
+				"userId": frm.todo_userId.value,
+				"title": frm.todo_title.value,
+				"completed": false
 			}),
 			headers: {
 				"Content-type": "application/json; charset=UTF-8"
 			}
 		}).then(response=>response.json())
-		//<input type='checkbox' id="checkbox_${res[i].id}" name="checkbox_${res[i].id}" onclick='check_func("${res[i].id}")'><label for="checkbox_${res[i].id}" id="text_${res[i].id}">${res[i].title}</label></input><br>
-		  .then(data=>docu_list.innerHTML += `<label class="container border border-primary mx-auto py-2 rounded-pill" for="checkbox_${data.id}">
-												  <input type='checkbox' id="checkbox_${data.id}" name="checkbox_${data.id}" onclick='check_func("${data.id}")'>
-														  <span id="text_${data.id}">${data.title}</span>
-													</input>
-											</label>`)
-	
-		}
+		  .then((data)=>{
+			  				console.log(data.id)
+			  				docu_list.innerHTML = `<label class="container border border-primary mx-auto py-2 rounded-pill" id="label_${data.id}" for="checkbox_${data.id}">
+														<input type='checkbox' id="checkbox_${data.id}" name="checkbox_${data.id}" onclick='check_func("${data.id}")'>
+															<span id="text_${data.id}">${data.title}</span>
+														</input>
+													</label>` + docu_list.innerHTML;
+			})
+	}
 }
